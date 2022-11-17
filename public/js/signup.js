34065924e6cff44c1b90d4e2ee4b95a9e446781a
Alexander <alexanders.tischner@outlook.com>
2022-11-16 17:06:37 -0700

    page routes

--- public/js/signup.js
+++ public/js/signup.js
@@ -26,4 +26,4 @@ async function signupFormHandler(event) {
     }
 }
   
-document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
+document.querySelector('#sign-up-button').addEventListener('submit', signupFormHandler);
